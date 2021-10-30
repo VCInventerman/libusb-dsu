1d89bf4d0a6a4f9d19785b0d2952a6d43aaa4db8
Nicholas Karr <nickkarr2005@gmail.com>
2021-10-30 10:16:40 -0500

    Add boost

--- main.cpp
+++ main.cpp
@@ -1,12 +1,15 @@
-#include <iostream>
-
-#include <libusb-1.0/libusb.h>
-
 #define EVG_LIB_BOOST
-
 #include <evglib>
+
+#ifdef EVG_PLATFORM_WIN
+#pragma warning( push )
+#pragma warning( disable : 4200 )
+#include <libusb.h>
+#pragma warning( pop )
+#endif
 
 
 int main(int, char**) {
     std::cout << "Hello, world!\n";
+    
 }
